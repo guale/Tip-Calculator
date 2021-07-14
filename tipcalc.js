4b550a1dfca145697fa7ea09d988dfe61883df30
guale <gualeshadow@gmail.com>
2021-07-14 12:19:37 -0500

    Integrate calculation scripts with the page

--- tipcalc.js
+++ tipcalc.js
@@ -1,36 +1,29 @@
-//input be the bill (Arons job)
-
-let bill = 500;
-
-//number of people at the table (Arons job)
-
-let numPep = 7;
-
-//quality
-
-let quality = 4;
-
-//ppl
+//Initialize a varaible used to modify the total tip in case of a large party and define said limit
 
 let largeParty = 0;
+const PARTY_SIZE = 7;
 
-//making an if statement for the number of people
+//Initialize varaibles to be used in the later functions
 
-if (numPep >= 7){
-    largeParty = (bill * 0.05);
+let bill = 0;
+let numPep = 0;
+let quality = 0;
+
+//Adds an extra 5% gratuity for large parties
+
+if (numPep >= PARTY_SIZE) {
+	largeParty = bill * 0.05;
 }
 
-//Tip=Base 10% +(quality of service 1-5)*2 + if guests 5 + 5%
-function tipTotal(bill){
-    return Math.round(((bill * 0.1) + (bill * (quality * 0.02)) + largeParty)*100)/100;
+//Calculates the tip with a base of 10%, adding 2% - 12% based on the quality of the service, and adds an additional 5% for large groups
+function tipTotal() {
+	return (
+		Math.round((bill * 0.1 + bill * (quality * 0.02) + largeParty) * 100) /
+		100
+	);
 }
 
-//tip stuff  
-let tip = tipTotal(bill);
-
-//Tip per person = tip/guests
-function tipPerPerson(){
-    return Math.round(tip/numPep*100)/100;
+// Divides the total tip by the number of people
+function tipPerPerson() {
+	return Math.round((tip / numPep) * 100) / 100;
 }
-
-let tipEach = tipPerPerson();

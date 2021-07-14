0729b140d614503d5622c6ac673f59ce543109bf
guale <gualeshadow@gmail.com>
2021-07-14 09:57:03 -0500

    set up queries

--- query.js
+++ query.js
@@ -1,3 +1,3 @@
-let bill = document.querySelector(#bill);
-let numPep = document.querySelector(#guest);
-let 
+let bill = document.querySelector("#bill");
+let numPep = document.querySelector("#guest");
+let quality = document.querySelector("#quality");

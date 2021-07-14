4000e76a479dc1407e1bc9322777c5b86606008b
BrittanyTaga <86418575+BrittanyTaga@users.noreply.github.com>
2021-07-14 08:57:01 -0700

    Add files via upload
    Edited js with math checks and 2 decimal places

---
+++ tipcalc.js
@@ -0,0 +1,36 @@
+//input be the bill (Arons job)
+
+let bill = 500;
+
+//number of people at the table (Arons job)
+
+let numPep = 7;
+
+//quality
+
+let quality = 4;
+
+//ppl
+
+let largeParty = 0;
+
+//making an if statement for the number of people
+
+if (numPep >= 7){
+    largeParty = (bill * 0.05);
+}
+
+//Tip=Base 10% +(quality of service 1-5)*2 + if guests 5 + 5%
+function tipTotal(bill){
+    return Math.round(((bill * 0.1) + (bill * (quality * 0.02)) + largeParty)*100)/100;
+}
+
+//tip stuff  
+let tip = tipTotal(bill);
+
+//Tip per person = tip/guests
+function tipPerPerson(){
+    return Math.round(tip/numPep*100)/100;
+}
+
+let tipEach = tipPerPerson();
